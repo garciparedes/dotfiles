d3ae0240cdf9bf279123f66c06395cde44cb8907
Sergio García Prado <sergio@garciparedes.me>
2021-01-13 21:47:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
-sudo apt install \
+add-apt-repository universe
+
+apt upgrade
+
+apt install -y \
   vim \
   tmux \
   fish \
-  git \
-  curl
+  git \ 
+  curl \
+  fd-find \
+  textlive-full \
+  ruby-full \
+  ruby-bundler \
+  gnome-shell-extensions \
+  gnome-sushi \
+  vim-gtk \
+  unrar \
+  npm \
+  nodejs \
+  bat \
+  ripgrep \
+  lm-sensors \
+  xclip \
+  fzf
   
   
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
